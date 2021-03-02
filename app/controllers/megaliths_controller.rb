7f2df7900c0e4a7615cf486000c577fb0a4f452c
Alex Marriott <marriottaog@gmail.com>
2021-03-02 17:25:41 +0000

    WIP getting megalith photo to save

--- app/controllers/megaliths_controller.rb
+++ app/controllers/megaliths_controller.rb
@@ -10,4 +10,23 @@ class MegalithsController < ApplicationController
   def new
     @megalith = Megalith.new
   end
+
+  def create
+    @megalith = Megalith.new megalith_params
+    if @megalith.save
+      if params[:megalith][:photos]
+        @megalith_photo = MegalithPhoto.new(user: current_user, megalith: Megalith.last)
+        @megalith_photo
+      end
+      redirect_to megalith_path(@megalith)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def megalith_params
+    params.require(:megalith).permit(:name, :category, :tagline, :description)
+  end
 end
