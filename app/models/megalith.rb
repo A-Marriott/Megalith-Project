004b280b9bc92c5429e86ceb69c4a1bfb4cbc036
Alex Marriott <71762355+A-Marriott@users.noreply.github.com>
2021-03-02 15:53:01 +0000

    Update megalith.rb

--- app/models/megalith.rb
+++ app/models/megalith.rb
@@ -3,10 +3,9 @@ class Megalith < ApplicationRecord
   has_many :megalith_photos
   has_many :favourites
   has_many :comments
+  has_many :trip_megaliths
   has_many :trips, through: :trip_megaliths
   has_many :visiteds
-  has_many :users, through: :visiteds
-  # has_many_attached :megalith_photos?
   validates :name, :category, :tagline, :description, presence: true
 
   # validates :name, uniqueness: true
