d5b450ca992aaf3e43f0e69be7aaadcf6b60d8b7
Alex Marriott <marriottaog@gmail.com>
2021-03-02 16:01:16 +0000

    added has many routes

--- app/models/megalith.rb
+++ app/models/megalith.rb
@@ -1,2 +1,9 @@
 class Megalith < ApplicationRecord
+  has_many :ratings
+  has_many :megalith_photos
+  has_many :favourites
+  has_many :comments
+  has_many :visiteds
+  has_many :trip_megaliths
+  has_many :trips, through: :trip_megaliths
 end
