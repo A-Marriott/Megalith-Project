cd912a7f3d7c423419bc0301bcbf7d53fab54ee1
Alex Marriott <marriottaog@gmail.com>
2021-03-03 13:19:57 +0000

    WIP

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,5 +5,22 @@ class TripsController < ApplicationController
   end
 
   def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      TripUser.create(user: current_user, trip: @trip)
+      TripMegalith.create(megalith: params[:megalith_id], trip: @trip, main: true)
+      redirect_to trip_path(@megalith)
+    elsif @megalith.valid?
+      flash[:alert] = "Please upload at least one photo"
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :tagline, :description)
   end
 end
