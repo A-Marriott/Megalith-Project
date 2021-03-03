8c8fb3b1f1053b8339a2651644cb731ee8dd5205
Todd <toddneale13@gmail.com>
2021-03-03 15:32:48 +0000

    update merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @megaliths = Megalith.all.first(10)
+    # classmethod to find top megaliths
+    # self.toprated
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import collapse from "../plugins/init_collapse.js" ;
 
 
 // Internal imports, e.g:
@@ -33,4 +34,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  collapse();
 });
