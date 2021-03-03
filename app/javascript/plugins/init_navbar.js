5ab58cc06d899294b35aed6ebffbf75efea8f88e
Todd <toddneale13@gmail.com>
2021-03-03 18:05:40 +0000

    ohh would you look at that

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -1,15 +1,16 @@
 const initUpdateNavbarOnScroll = () => {
-
-    const navbar = document.querySelector('.navbar-megalithic');
-    if (navbar) {
-      window.addEventListener('scroll', () => {
-        if (window.scrollY >= window.innerHeight) {
-          navbar.classList.add('navbar-megalithic-transparent');
-        } else {
-          navbar.classList.remove('navbar-megalithic-transparent');
-        }
-      });
-    }
+  const navbar = document.querySelector('.navbar-megalithic');
+  let scroll = window.pageYOffset || document.body.scrollTop;
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      const currentScroll = window.pageYOffset || document.body.scrollTop;
+      if (currentScroll > scroll) {
+        navbar.classList.add('navbar-megalithic-transparent');
+      } else {
+        navbar.classList.remove('navbar-megalithic-transparent');
+      }
+      scroll = currentScroll <= 0 ? 0 : currentScroll;
+    }, false);
+  }
 }
-
 export { initUpdateNavbarOnScroll };
