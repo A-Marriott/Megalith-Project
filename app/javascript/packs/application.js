278a2f6118821e3e500a94e9f1c52572dc50a91b
Marc Appadoo <marcappadoo@yahoo.co.uk>
2021-03-03 17:14:16 +0000

    added a map with info-window and custom markers, arfa not too fond of emojis however

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -33,4 +33,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
