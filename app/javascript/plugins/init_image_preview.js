9fbfe1b14e16d71e209602723af331357732c8c4
Alex Marriott <marriottaog@gmail.com>
2021-03-04 16:45:14 +0000

    WIP multiple image preview

--- app/javascript/plugins/init_image_preview.js
+++ app/javascript/plugins/init_image_preview.js
@@ -8,13 +8,21 @@ const previewImageOnFileSelect = () => {
 }
 
 const displayPreview = (input) => {
+  let count = 0;
   if (input.files && input.files[0]) {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      document.getElementById('photo-preview').src = event.currentTarget.result;
+    for (const [key, value] of Object.entries(input.files)) {
+      document.getElementById('photo_upload_form').insertAdjacentHTML('afterend', `<img src="" class="hidden" width="200" id="photo-preview-${count}">`);
+
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        document.getElementById(`photo-preview-${count}`).src = event.currentTarget.result;
+      }
+      reader.readAsDataURL(value)
+      document.getElementById(`photo-preview-${count}`).classList.remove('hidden');
+
+      count += 1;
     }
-    reader.readAsDataURL(input.files[0])
-    document.getElementById('photo-preview').classList.remove('hidden');
   }
 }
 
