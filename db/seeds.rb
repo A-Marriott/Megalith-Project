bda11d0503982665fed76d3f4c720584b2dd9593
Arthur Campbell <arfa.camble@gmail.com>
2021-03-05 18:58:44 +0000

    filename changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ brian = User.create(email: 'brian@internet.com', password: 'password', username:
 noobles = User.create(email: 'rocknoob@test.com', password: 'password', username: 'Noob on the Rocks')
 photoadmin = User.create(email: 'photographer@test.com', password: 'password', username: 'Stone cold Snapper')
 
-json_file = File.join(File.dirname(__FILE__), "./seed-play/Dorset-latlong.json")
+json_file = File.join(File.dirname(__FILE__), "./seed-play/Dorset-v3-latlong-formatted.json")
 liths = JSON.parse(File.read(json_file))
 liths.first(20).each do |lith|
   new_lith = Megalith.new(
