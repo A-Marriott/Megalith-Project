ebdb24bdf42fb4a51ca7bac4e25fd66a8877aabb
Alex Marriott <marriottaog@gmail.com>
2021-03-08 12:32:56 +0000

    made variable names in controller to simplify logic in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
+    @comments = @user.comments
+    @photos = @user.megalith_photos
+    @favourites = @user.favourited
+    @visiteds = @user.visited
   end
 end
