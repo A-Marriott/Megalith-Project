3beb0ea03325f5e3ed3ea3456713d2cd9b38a311
Alex Marriott <marriottaog@gmail.com>
2021-03-08 12:48:20 +0000

    profile page has necessary logic to grab planned and completed trips

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
     @photos = @user.megalith_photos
     @favourites = @user.favourited
     @visiteds = @user.visited
+    @planned_trips = @user.trips.where(date_visited: nil)
+    @completed_trips = @user.trips.where.not(date_visited: nil)
   end
 end
