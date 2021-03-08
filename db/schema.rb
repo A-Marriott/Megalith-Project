cbc2b0d467415cd8c3e2d3f3c0979f6d1fd9b804
Arthur Campbell <arfa.camble@gmail.com>
2021-03-08 10:28:23 +0000

    schema whatever

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_07_151433) do
 
   # These are extensions that must be enabled in order to support this database
