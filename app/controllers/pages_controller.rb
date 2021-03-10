e50296df9e5383472eb6b45f4c06a54e1a85705f
Arthur Campbell <arfa.camble@gmail.com>
2021-03-10 09:58:11 +0000

    tiny debugging and tiny seed change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @megaliths = Megalith.all.sort { |a,b| a && b ? a <=> b : a ? 1 : -1 }.reverse
+    @megaliths = Megalith.all
+                         .sort { |a,b| a.average_rating && b.average_rating ? a.average_rating <=> b.average_rating : a.average_rating ? 1 : -1 }
+                         .reverse
+                         .first(10)
   end
 end
