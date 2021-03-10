59f2f580be70340bd0f79ba048d1e630bb4e36db
Arthur Campbell <arfa.camble@gmail.com>
2021-03-10 10:46:12 +0000

    marker for show page

--- app/controllers/megaliths_controller.rb
+++ app/controllers/megaliths_controller.rb
@@ -32,7 +32,7 @@ class MegalithsController < ApplicationController
       @average_rating = @megalith.average_rating
       @trip = Trip.new
       @trips = @megalith.trips.where(published: true)
-      @markers = [{lat:@megalith.latitude, lng:@megalith.longitude}]
+      @markers = [{lat:@megalith.latitude, lng:@megalith.longitude, image_url: helpers.asset_url('logo.png')}]
       @comment = Comment.new
       @comment.megalith_id = @megalith.id
     end
