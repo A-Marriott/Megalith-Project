860d26381d10fb5af2ea333b0649926efd07cf3e
Arthur Campbell <arfa.camble@gmail.com>
2021-03-11 17:13:28 +0000

    tiny wee bits

--- arthur_storage.rb
+++ arthur_storage.rb
@@ -2,4 +2,14 @@ stone = Megalith.where(name: 'Cross and Hand Stone').first
 
 trip = stone.trips.first
 
-trip.update(date_visited: nil, published: false)
+trip.update(date_visited: nil, published: false, top_tip: nil, tagline: nil, description: "let's see this rock and then have a few pints")
+
+trip.trip_photos.each {|p| p.destroy}
+
+brian = User.first
+
+badbury = Megalith.find_by_name('Badbury Rings')
+
+visited = Visited.where(user: brian, megalith: badbury).first
+
+visited.destroy
