c8cee45161100e280b9cd91ac5f9a268aa37fe5a
Arthur Campbell <arfa.camble@gmail.com>
2021-03-12 17:30:51 +0000

    last storage changes

--- arthur_storage.rb
+++ arthur_storage.rb
@@ -3,6 +3,7 @@ trip = stone.trips.first
 trip.update(date_visited: nil, published: false, top_tip: nil, tagline: nil, description: "let's see this rock and then have a few pints")
 stone.megalith_photos.select {|p| p.photo_key }.each {|p| p.destroy}
 trip.trip_photos.each {|p| p.destroy}
+trip.get_likes.each { |l| l.destroy }
 
 visited1 = Visited.where(user: brian, megalith: stone).first
 visited1.destroy
@@ -16,7 +17,3 @@ badbury.trips.last.destroy
 badbury.megalith_photos.first.disliked_by brian
 visited2 = Visited.where(user: brian, megalith: badbury).first
 visited2.destroy
-
-
-badbury = Megalith.find_by_name('badbury rings')
-badbury.megalith_photos.first.get_likes.last.destroy
