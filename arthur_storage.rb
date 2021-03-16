ebf4c87c99533869ffc3e477ea5c35cb48803020
Arthur Campbell <arfa.camble@gmail.com>
2021-03-16 20:05:09 +0000

    small change to storage file before demo

--- arthur_storage.rb
+++ arthur_storage.rb
@@ -5,6 +5,7 @@ stone.megalith_photos.select {|p| p.photo_key }.each {|p| p.destroy}
 trip.trip_photos.each {|p| p.destroy}
 trip.get_likes.each { |l| l.destroy }
 
+brian = User.first
 visited1 = Visited.where(user: brian, megalith: stone).first
 visited1.destroy
 
@@ -17,3 +18,6 @@ badbury.trips.last.destroy
 badbury.megalith_photos.first.disliked_by brian
 visited2 = Visited.where(user: brian, megalith: badbury).first
 visited2.destroy
+
+Trip.find_by_tagline('Rush').disliked_by User.first
+Trip.find_by_tagline('The Allman Brothers').disliked_by User.first
