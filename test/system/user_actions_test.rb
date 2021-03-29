abf23073946866aa40cebb5670eb4a50c2c73965
Arthur Campbell <arfa.camble@gmail.com>
2021-03-29 14:47:19 +0100

    second test written and passed

--- test/system/user_actions_test.rb
+++ test/system/user_actions_test.rb
@@ -7,4 +7,14 @@ class UserActionsTest < ApplicationSystemTestCase
     assert_text "Login"
   end
 
+  test "navigating to megalith page displays a favourite button" do
+    visit 'users/sign_in'
+    fill_in "user[email]", with: "test@user.com"
+    fill_in "user[password]", with: "password"
+    click_on "Log in"
+    Megalith.create(name: "Stonehenge")
+    id = Megalith.first.id
+    visit "megaliths/#{id}"
+    assert_text "Mark as visited"
+  end
 end
