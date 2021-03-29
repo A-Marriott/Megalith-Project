7e09cc42dd328b5515839d6d2a21e1e5d6adb514
Arthur Campbell <arfa.camble@gmail.com>
2021-03-29 16:26:10 +0100

    tests conditional on user actions

--- test/system/user_actions_test.rb
+++ test/system/user_actions_test.rb
@@ -7,7 +7,7 @@ class UserActionsTest < ApplicationSystemTestCase
     assert_text "Login"
   end
 
-  test "navigating to megalith page displays a favourite button" do
+  test "navigating to megalith page displays mark as visited button" do
     visit 'users/sign_in'
     fill_in "user[email]", with: "test@user.com"
     fill_in "user[password]", with: "password"
@@ -16,5 +16,28 @@ class UserActionsTest < ApplicationSystemTestCase
     id = Megalith.first.id
     visit "megaliths/#{id}"
     assert_text "Mark as visited"
+  end
+
+  test "megalith page does not show rating form when not marked as visited" do
+    visit 'users/sign_in'
+    fill_in "user[email]", with: "test@user.com"
+    fill_in "user[password]", with: "password"
+    click_on "Log in"
+    Megalith.create(name: "Stonehenge")
+    id = Megalith.first.id
+    visit "megaliths/#{id}"
+    assert has_no_selector? "#new_rating"
+  end
+
+  test "megalith page shows rating form when marked as visited" do
+    visit 'users/sign_in'
+    fill_in "user[email]", with: "test@user.com"
+    fill_in "user[password]", with: "password"
+    click_on "Log in"
+    Megalith.create(name: "Stonehenge")
+    id = Megalith.first.id
+    visit "megaliths/#{id}"
+    find_link("Mark as visited").click
+    assert has_selector? "#new_rating"
   end
 end
